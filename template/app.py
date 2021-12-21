afb2cf0609d5580bff55ec7526e32bb317282e42
Prachi Mohare <prachi.250801@gmail.com>
2021-12-21 10:49:23 +0530

    main

--- template/app.py
+++ template/app.py
@@ -18,48 +18,27 @@ from gevent.pywsgi import WSGIServer
 
 # Define a flask app
 app = Flask(__name__,template_folder='template')
-
-# Model saved with Keras model.save()
-MODEL_PATH = 'D:/dogs-vs-cats-redux-kernels-edition/catsdogs.h5'
-# Load your trained model
+################################################################
+################################################################
+MODEL_PATH = 'Dogs-vs-Cats_model.h5'
 model = load_model(MODEL_PATH)
-# print('Model loaded. Start serving...')
-# You can also use pretrained model from Keras
-# Check https://keras.io/applications/
-#from keras.applications.resnet50 import ResNet50
-#model = ResNet50(weights='imagenet')
-#model.save('')
-print('Model loaded. Check http://127.0.0.1:5000/')
-
-
 def model_predict(img_path, model):
     img = image.load_img(img_path, target_size=(150, 150))
-
     # Preprocessing the image
     x = image.img_to_array(img)
     # x = np.true_divide(x, 255)
     x = np.expand_dims(x, axis=0)
-
-    # Be careful how your trained model deals with the input
-    # otherwise, it won't make correct prediction!
-    # x = preprocess_input(x, mode='caffe')
-
     preds = model.predict(x)
     return preds
-
-
 @app.route('/', methods=['GET'])
 def index():
     # Main page
     return render_template('index.html')
-
-
 @app.route('/predict', methods=['GET', 'POST'])
 def upload():
     if request.method == 'POST':
         # Get the file from post request
         f = request.files['file']
-
         # Save the file to ./uploads
         basepath = os.path.dirname(__file__)
         file_path = os.path.join(
@@ -67,7 +46,6 @@ def upload():
         f.save(file_path)
         print("\n\n\n")
         print(type(f))
-
         # Make prediction
         preds = model_predict(file_path, model)
         print(preds)
